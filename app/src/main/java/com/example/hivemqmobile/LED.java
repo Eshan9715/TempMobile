c2afbfec78a6d38ab6f0f8a39fca988033ca6822
Eshan Amanda <eshanesn97@gmail.com>
2022-05-31 14:38:49 +0530

    Add Settings UI

--- app/src/main/java/com/example/hivemqmobile/LED.java
+++ app/src/main/java/com/example/hivemqmobile/LED.java
@@ -7,14 +7,51 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.google.android.material.card.MaterialCardView;
 
 
 public class LED extends Fragment {
+    View v;
+    MaterialCardView onB, offB;
+    ImageView bulbON, bulbOFF;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_l_e_d, container, false);
+        v = inflater.inflate(R.layout.fragment_l_e_d, container, false);
+
+        onB = v.findViewById(R.id.l1);
+        offB = v.findViewById(R.id.l2);
+        bulbON = v.findViewById(R.id.ledOn);
+        bulbOFF = v.findViewById(R.id.ledOff);
+
+        bulbON.setVisibility(View.INVISIBLE);
+
+        onB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bulbOFF.setVisibility(View.INVISIBLE);
+                bulbON.setVisibility(View.VISIBLE);
+
+
+            }
+        });
+
+        offB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bulbON.setVisibility(View.INVISIBLE);
+                bulbOFF.setVisibility(View.VISIBLE);
+            }
+        });
+
+
+
+
+
+        return v;
     }
 }
--- app/src/main/java/com/example/hivemqmobile/Temp.java
+++ app/src/main/java/com/example/hivemqmobile/Temp.java
@@ -7,13 +7,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class Temp extends Fragment {
+    View v;
+    TextView temCel, temFra;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_temp, container, false);
+        v = inflater.inflate(R.layout.fragment_temp, container, false);
+
+        temCel = v.findViewById(R.id.room_cel);
+        temFra = v.findViewById(R.id.room_fah);
+
+
+        return  v;
     }
 }
